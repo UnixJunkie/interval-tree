ff9ca36bfd76e9d8f6c7b0ee065ac458de843b6f
Francois Berenger <berenger@riken.jp>
2012-02-15 10:13:39 +0900

    should be finished

--- interval_tree.ml
+++ interval_tree.ml
@@ -31,54 +31,24 @@
  You should have received a copy of the GNU Lesser General Public License
  along with this program. If not, see <http://www.gnu.org/licenses/>. *)
 
-module A    = Array
-module B    = Buffer
-module BL   = BatList
-module L    = List
-module MU   = My_utils
-module P    = Printf
+module A = Array
+module L = List
 
-type 'a interval = { lbound : float ;
-                     rbound : float ;
-                     data   : 'a    }
+type interval = { lbound : float ;
+                  rbound : float }
 
-type 'a interval_tree =
+type interval_tree =
     Empty
   | Node of
-   (* x_mid   left_list          right_list *)
-      float * 'a interval list * 'a interval list *
-   (* left_tree          right_tree *)
-      'a interval_tree * 'a interval_tree
+   (* x_mid   left_list       right_list      left_tree       right_tree *)
+      float * interval list * interval list * interval_tree * interval_tree
 
-let new_interval l r d =
+(* -------------------- utility functions -------------------- *)
+
+let new_interval l r =
   assert (l <= r);
   { lbound = l ;
-    rbound = r ;
-    data   = d }
-
-let string_of_interval interval =
-  P.sprintf "(%f, %f)" interval.lbound interval.rbound
-
-let string_of_intervals intervals =
-  MU.string_of_list string_of_interval "; " intervals
-
-let string_of_tree tree =
-  let buff = B.create 1024 in
-  let rec string_of_tree_priv level tree =
-    match tree with
-        Empty -> ()
-      | Node (x_mid, left_list, right_list, left_tree, right_tree) ->
-          B.add_string buff
-            (P.sprintf "level: %d\nx_mid: %f\nll: %s\nrl: %s\n"
-               level
-               x_mid
-               (string_of_intervals left_list)
-               (string_of_intervals right_list));
-          string_of_tree_priv (level + 1) left_tree;
-          string_of_tree_priv (level + 1) right_tree
-  in
-  string_of_tree_priv 0 tree;
-  B.contents buff
+    rbound = r }
 
 let leftmost_bound_first i1 i2 =
   compare i1.lbound i2.lbound
@@ -103,9 +73,17 @@ let bounds_array_of_intervals intervals =
     intervals;
   res
 
+let median xs =
+  A.sort compare xs;
+  let n = A.length xs in
+  if n mod 2 = 1 then
+    xs.(n/2)
+  else
+    (xs.(n/2) +. xs.(n/2 - 1)) /. 2.0
+
 let median intervals =
   let bounds = bounds_array_of_intervals intervals in
-  MU.median_a bounds
+  median bounds
 
 let partition intervals x_mid =
   let left_intervals, maybe_right_intervals =
@@ -118,12 +96,13 @@ let partition intervals x_mid =
       maybe_right_intervals in
    left_intervals, mid_intervals, right_intervals
 
+(* -------------------- construction -------------------- *)
+
 (* interval tree of a list of intervals WARNING: NOT TAIL RECURSIVE *)
 let rec interval_tree intervals =
   match intervals with
       [] -> Empty
     | _  ->
-        (* FBR: intervals could be stored in two sets for speed *)
         let x_mid            = median intervals                 in
         let left, mid, right = partition intervals x_mid        in
         let left_list        = L.sort leftmost_bound_first  mid in
@@ -132,25 +111,27 @@ let rec interval_tree intervals =
               left_list, right_list,
               interval_tree left, interval_tree right)
 
-let interval_tree_of_pairs pairs =
-  let i = ref 0 in
-  interval_tree
-    (L.fold_left
-       (fun acc (a, b) ->
-          let res = (new_interval a b !i) :: acc in
-          incr i;
-          res)
-       [] pairs)
+(* -------------------- query -------------------- *)
+
+(* fold_left f on l while p is true *)
+let rec fold_while f p acc l =
+  match l with
+      []      -> acc
+    | x :: xs ->
+        if p x then
+          fold_while f p (f x :: acc) xs
+        else
+          acc
 
 let filter_left_list l qx acc =
-  MU.fold_while
-    MU.identity
+  fold_while
+    (fun x -> x)
     (fun interval -> interval.lbound <= qx)
     acc l
 
 let filter_right_list l qx acc =
-  MU.fold_while
-    MU.identity
+  fold_while
+    (fun x -> x)
     (fun interval -> interval.rbound >= qx)
     acc l
 
@@ -168,31 +149,23 @@ let query initial_tree qx =
   in
   query_priv [] initial_tree
 
-let data_of_query_result tree qx =
-  let res = query tree qx in
-  BL.map (fun itv -> itv.data) res
-
-(* -------------------- tests -------------------- *)
+(* -------------------- tests in the toplevel -------------------- *)
 
 (*
-let main () =
-  let tree = interval_tree_of_pairs [( 0.,  4.);
-                                     ( 1.,  2.);
-                                     ( 3.,  9.);
-                                     ( 5.,  7.);
-                                     ( 6., 13.);
-                                     ( 8., 11.);
-                                     (10., 12.)] in
-  P.printf "%s\n" (string_of_tree tree);
-  let _intervals = [new_interval  0.  4.;
-                    new_interval  1.  2.;
-                    new_interval  3.  9.;
-                    new_interval  5.  7.;
-                    new_interval  6. 13.;
-                    new_interval  8. 11.;
-                    new_interval 10. 12.] in ();
-  P.printf "%s\n" (string_of_intervals (query tree (MU.atof Sys.argv.(1))))
-;;
 
-main()
+let interval_tree_of_pairs pairs =
+  interval_tree
+    (L.fold_left
+       (fun acc (a, b) -> (new_interval a b) :: acc)
+       [] pairs)
+
+let tree = interval_tree_of_pairs [ 0.,  4.;
+                                    1.,  2.;
+                                    3.,  9.;
+                                    5.,  7.;
+                                    6., 13.;
+                                    8., 11.;
+                                   10., 12.];;
+query tree 0.5;;
+
 *)
