7e19459f7556331b329ded281d252ecbd0f4b955
Francois Berenger <berenger@riken.jp>
2014-03-24 15:19:40 +0900

    licensed was changed

--- lib/interval_tree.ml
+++ lib/interval_tree.ml
@@ -12,24 +12,7 @@
      doi       = "10.1007/978-3-540-77974-2",
      year      = "2008",
      publisher = "Springer"
-   }
-
- Copyright (C) 2012 Zhang Initiative Research Unit,
- Advance Science Institute, RIKEN
- 2-1 Hirosawa, Wako, Saitama 351-0198, Japan
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public License
- along with this program. If not, see <http://www.gnu.org/licenses/>. *)
+   } *)
 
 module A = Array
 module L = List
