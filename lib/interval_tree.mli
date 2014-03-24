a689775844407b5d9620336be09e71dceefd0342
Francois Berenger <berenger@riken.jp>
2014-03-24 18:06:07 +0900

    added interface and ocamldoc

--- lib/interval_tree.mli
+++ lib/interval_tree.mli
@@ -1,12 +1,34 @@
 
+type t
+
 module Interval : sig
-  type t
+
+  (** An interval has a left and a right bound *)
+  type t = { lbound : float ;
+             rbound : float }
+
+  (** [create lbound rbound] create a new float interval.
+      PRECONDITION: [lbound] must be <= [rbound]. *)
   val create : float -> float -> t
+
+  (** [of_pair (lbound, rbound)] = [create lbound rbound] *)
+  val of_pair : float * float -> t
+
+  (** [to_pair itv] = [(lbound, rbound)] *)
   val to_pair : t -> float * float
 end
 
-type t
+(** {4 Constructors} *)
 
-val create   : Interval.t list -> t
+(** [create intervals_list] : interval tree of all intervals in the list *)
+val create : Interval.t list -> t
+
+(** [of_pairs intervals_bounds_pairs] : interval tree of all intervals
+    whose bounds are given in a list of bounds pairs *)
 val of_pairs : (float * float) list -> t
-val query    : t -> float -> Interval.t list
+
+(** {4 Query} *)
+
+(** [query tree q] : list of intervals in the tree [t] containing
+    the float [q] *)
+val query : t -> float -> Interval.t list
