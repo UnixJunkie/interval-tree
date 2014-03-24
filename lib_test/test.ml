3252a7c2e4bb8b2b0e5034c1d10cbf1c92cf1355
Francois Berenger <berenger@riken.jp>
2014-03-24 17:20:22 +0900

    more tests and easier to maintain

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -3,15 +3,30 @@ module Itvt = Interval_tree
 module Itv  = Itvt.Interval
 module L    = List
 
-let tree = Itvt.of_pairs [ 0.,  4.;
-                           1.,  2.;
-                           3.,  9.;
-                           5.,  7.;
-                           6., 13.;
-                           8., 11.;
-                           10., 12.];;
-Itvt.query tree 0.5;;
+let sort_intervals l =
+  L.sort
+    (fun ((lb1 : float), _rb1) ((lb2 : float), _rb2) -> compare lb1 lb2)
+    l
 
-(* FBR:
- - to_pairs
-*)
+let main () =
+  let interval_bounds = [ 0.,   4.;
+                          1.,   2.;
+                          3.,   9.;
+                          5.,   7.;
+                          6.,  13.;
+                          8.,  11.;
+                          10., 12.] in
+  let tree = Itvt.of_pairs interval_bounds in
+  let test q =
+    sort_intervals (L.map Itv.to_pair (Itvt.query tree q))
+  in
+  assert([(0., 4. )]                        = test 0.5 );
+  assert([(0., 4. ); (1., 2. )]             = test 1.0 );
+  assert([(0., 4. ); (3., 9. )]             = test 3.0 );
+  assert([(3., 9. ); (5., 7. )]             = test 5.0 );
+  assert([(3., 9. ); (5., 7. ); (6., 13. )] = test 6.0 );
+  assert([(3., 9. ); (6., 13.); (8., 11. )] = test 8.0 );
+  assert([(6., 13.); (8., 11.); (10., 12.)] = test 10.0);
+;;
+
+main()
