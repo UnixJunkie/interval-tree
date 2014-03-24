c0925b49a08340a88de7e0eed7288c9167382f47
Francois Berenger <berenger@riken.jp>
2014-03-24 15:21:04 +0900

    now compiles added some TODOs

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -1,17 +1,28 @@
 
-let Itv_tree = Interval_tree
+module Itvt = Interval_tree
+module L    = List
 
 let of_pairs pairs =
-  interval_tree
+  Itvt.interval_tree
     (L.fold_left
-       (fun acc (a, b) -> (new_interval a b) :: acc)
+       (fun acc (a, b) -> (Itvt.new_interval a b) :: acc)
        [] pairs)
 
-let tree = interval_tree_of_pairs [ 0.,  4.;
-                                    1.,  2.;
-                                    3.,  9.;
-                                    5.,  7.;
-                                    6., 13.;
-                                    8., 11.;
-                                   10., 12.];;
-query tree 0.5;;
+let tree = of_pairs [ 0.,  4.;
+                      1.,  2.;
+                      3.,  9.;
+                      5.,  7.;
+                      6., 13.;
+                      8., 11.;
+                      10., 12.];;
+Itvt.query tree 0.5;;
+
+(* FBR:  *)
+
+(* Itvt.create *)
+
+(* Itvt.Itv.create *)
+
+(* of_pairs *)
+
+(* to_pairs *)
