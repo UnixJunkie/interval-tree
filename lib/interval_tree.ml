5dc79512a358d4e214617cde929b1ea8cf3e92a9
Francois Berenger <berenger@riken.jp>
2014-03-24 15:09:36 +0900

    moved tests into ../lib_test/test.ml

--- lib/interval_tree.ml
+++ lib/interval_tree.ml
@@ -148,24 +148,3 @@ let query initial_tree qx =
           query_priv new_acc right_tree
   in
   query_priv [] initial_tree
-
-(* -------------------- tests in the toplevel -------------------- *)
-
-(*
-
-let interval_tree_of_pairs pairs =
-  interval_tree
-    (L.fold_left
-       (fun acc (a, b) -> (new_interval a b) :: acc)
-       [] pairs)
-
-let tree = interval_tree_of_pairs [ 0.,  4.;
-                                    1.,  2.;
-                                    3.,  9.;
-                                    5.,  7.;
-                                    6., 13.;
-                                    8., 11.;
-                                   10., 12.];;
-query tree 0.5;;
-
-*)
