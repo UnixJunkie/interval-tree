97fcefa1fce0f54d271a34f9bfdc1e6f1957fed1
Francois BERENGER <francois.berenger@inria.fr>
2016-06-23 10:19:32 +0200

    Johan forgot to add to_triplets in the interface

--- lib/interval_tree.mli
+++ lib/interval_tree.mli
@@ -11,26 +11,30 @@ module Interval : sig
   (** [create lbound rbound value] create a new float interval
       with an associated value.
       PRECONDITION: [lbound] must be <= [rbound]. *)
-  val create : float -> float -> 'a -> 'a t
+  val create: float -> float -> 'a -> 'a t
 
   (** [of_triplet (lbound, rbound, value)] = [create lbound rbound value] *)
-  val of_triplet : float * float * 'a -> 'a t
+  val of_triplet: float * float * 'a -> 'a t
 
   (** [to_triplet itv] = [(lbound, rbound, value)] *)
-  val to_triplet : 'a t -> float * float * 'a
+  val to_triplet: 'a t -> float * float * 'a
 end
 
 (** {4 Constructors} *)
 
-(** [create intervals_list] : interval tree of all intervals in the list *)
-val create : 'a Interval.t list -> 'a t
+(** [create intervals_list]: interval tree of all intervals in the list *)
+val create: 'a Interval.t list -> 'a t
 
-(** [of_triplets interval_triplets] : interval tree of all intervals
+(** [of_triplets interval_triplets]: interval tree of all intervals
     whose bounds and values are given in a list *)
-val of_triplets : (float * float * 'a) list -> 'a t
+val of_triplets: (float * float * 'a) list -> 'a t
+
+(** list all intervals and values in the tree
+    WARNING: not tail recursive *)
+val to_triplets: 'a t -> (float * float * 'a) list
 
 (** {4 Query} *)
 
-(** [query tree q] : list of intervals in the tree [t] containing
+(** [query tree q]: list of intervals in the tree [t] containing
     the float [q] *)
-val query : 'a t -> float -> 'a Interval.t list
+val query: 'a t -> float -> 'a Interval.t list
